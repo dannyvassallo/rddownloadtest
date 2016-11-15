77a76c827e156a8c894cc283b1cf77467c4e63b7
dannyvassallo <danielvassallo87@gmail.com>
2016-11-15 12:11:56 -0500

    setup port for heroku

--- server.js
+++ server.js
@@ -2,12 +2,14 @@ var express = require('express');
 var app = express();
 var path = require('path');
 
+var PORT = process.env.PORT || 8080;
+
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname + '/public/views/index.html'));
 })
 
-app.listen(3000, function () {
+app.listen(PORT, function () {
   console.log('Example app listening on port 3000!');
 })
